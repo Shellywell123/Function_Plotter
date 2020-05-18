c5908c27118fbdd2435fa5a9537632c6cd386de1
Shellywell123 <benjamin@shellswell.com>
2020-05-18 15:56:04 +0100

    removed old func

--- run3d.py
+++ run3d.py
@@ -2,43 +2,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from numpy import *
 
-def runOld():
-    """
-    """
-    func_string = input('input your function:\n')
-
-    #lims preset fo now
-    xfunc_lims = (-100,100)
-    zfunc_lims = (-100,100)
-
-    xlist = linspace(xfunc_lims[0],xfunc_lims[1],1000)
-    zlist = linspace(zfunc_lims[0],zfunc_lims[1],1000)
-    ylist = []
-
-    for n in range (0,len(xlist)):
-        x = xlist[n]
-        z = zlist[n]
-
-        try:
-            y = eval(func_string)
-            ylist.append(y)
-        except:
-            print('invalid input retry:')
-            run()
-
-    fig = plt.figure('Function_Plotter')
-    ax = fig.add_subplot(111, projection='3d')
-    plt.title('$y = '+func_string+'$')
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$y$')
-    ax.set_zlabel('$z$')
-    if 'z' in func_string:
-        ax.plot(xlist,ylist,zlist)
-    else:
-        ax.plot(xlist,ylist)
-    plt.show()
-
-
 def run():
     """
     """
