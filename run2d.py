dbb0cd2fff45194085f407379408674254c7f8a4
Shellywell123 <benjamin@shellswell.com>
2020-05-20 18:26:29 +0100

    gen fixes + lims into in 2d

--- run2d.py
+++ run2d.py
@@ -10,6 +10,8 @@ input function in forms,
 y = f(x)
 x = f(y)
 
+limit input example
+y = x**2 #0<x<1 (can only set lim on x atm)
 Other Commands
 exit: quits program
 help: opens this page
@@ -25,14 +27,51 @@ def run():
     fvar = 'y'
     func_string_raw = input('input your function:\n')
 
-    #lims preset for now
+    
     xfunc_lims = (-100,100)
+    yfunc_lims = (-100,100)
+
+    if '#' in func_string_raw:
+        limits = func_string_raw.split('#')[1:]  
+        #print (limits)
+        for lim in limits:
+          #  print(lim.split('<'))
+
+          #  if (len(lim.split('<')) != 3) or (len(lim.split('>')) != 3):
+          #      print('invalid limits, retry:8')
+          #      run()
+
+            if ('<' in lim) and ( '>' in lim):
+                print('invalid limits, retry:')
+                run()
+
+            if '<' in lim:
+                lo,var,up = lim.split('<')
+                
+
+            if '>' in lim:
+                up,var,lo= lim.split('>')
+               # print(a)
+
+            if var == 'x':
+                print ('xhigh ={}, xlow={}'.format(up,lo))
+                xfunc_lims = (float(lo),float(up))
+
+      #      if var == 'y':
+      #          func_string = func_string_raw.split('=')[1]
+      #          x=float(up)
+      #          upy = eval(func_string)
+      #          x=float(lo)
+      #          loy = eval(func_string)
+      #          print ('yhigh ={}, ylow={}'.format(up,lo))
+      #          yfunc_lims = (float(loy),float(upy))
 
     if func_string_raw == 'help':
         help_info()
 
     if func_string_raw == 'exit':
         return 0
+        exit()
 
     if 'z' in func_string_raw:
         print ('2D is only in x & y, retry:')
@@ -61,7 +100,7 @@ def run():
     if ('x =' in func_string_raw) or ('x=' in func_string_raw):
         fvar = 'x'
         func_string = func_string_raw.split('=')[1]
-        ylist = linspace(xfunc_lims[0],xfunc_lims[1],1000)
+        ylist = linspace(yfunc_lims[0],yfunc_lims[1],1000)
         xlist = []
 
         for n in range (0,len(ylist)):
@@ -69,7 +108,7 @@ def run():
 
             try:
                 x = eval(func_string)
-                xlist.append(y)
+                xlist.append(x)
             except:
                 print('invalid input, type "help" or retry:')
                 run()
